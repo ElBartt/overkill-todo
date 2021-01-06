888631ceed99926c11a8f0be42307bfe07661fd4
arnauld <arnauld.alex@gmail.com>
2021-01-06 14:44:12 +0100

    fix basic test

--- src/app/components/todolist/todolist.component.spec.ts
+++ src/app/components/todolist/todolist.component.spec.ts
@@ -1,4 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { TodoListService } from 'src/app/service/todo-list.service';
+import { reducers } from 'src/app/state/state.model';
+import { TodoEffect } from 'src/app/state/todo.effect';
 
 import { TodolistComponent } from './todolist.component';
 
@@ -8,7 +21,20 @@ describe('TodolistComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TodolistComponent ]
+      declarations: [ TodolistComponent ],
+      imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatCheckboxModule,
+        MatListModule,
+        MatInputModule,
+        MatButtonModule,
+        StoreModule.forRoot(reducers),
+        EffectsModule.forRoot([TodoEffect])
+      ],
+      providers: [TodoListService]
     })
     .compileComponents();
   });
